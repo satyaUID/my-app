96130c57caa0e6c8fd87ed26b33548153ea34cc1
satyaUID <satyaveer.kashyap85@gmail.com>
2023-08-03 00:35:09 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       APP is working
+      </header>
+    </div>
+  );
+}
+
+export default App;
